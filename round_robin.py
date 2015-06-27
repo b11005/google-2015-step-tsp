c3ade42d4ad0163730b4c641a89545e39cb10b09
b11005 <instantjoy3@gmail.com>
2015-06-27 12:59:27 +0900

    総当たり

--- round_robin.py
+++ round_robin.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-import sys
 import math
+import sys
 
-from common import print_solution, read_input
+from common import print_solution, read_input, format_solution
 
 
 def distance(city1, city2):
@@ -12,24 +12,34 @@ def distance(city1, city2):
 
 def solve(cities):
     N = len(cities)
-
+    total=0
+    mini=10000000
     dist = [[0] * N for i in range(N)]
     for i in range(N):
         for j in range(N):
             dist[i][j] = dist[j][i] = distance(cities[i], cities[j])
-    print (dist)
+            total+=dist[i][j]
+        if total!=0 and mini>total: mini=total
+    print (mini)
+    
     current_city = 0
     unvisited_cities = set(range(1, N))
     solution = [current_city]
 
-    def distance_from_current_city(to):
-        return dist[current_city][to]
+    """def distance_from_current_city(to):
+        return dist[current_city][to]"""
 
     while unvisited_cities:
-        next_city = min(unvisited_cities, key=distance_from_current_city)
+        #next_city = min(unvisited_cities, key=distance_from_current_city)
+        if total!=0 and mini>total: 
+            mini=total
+        for i in unvisited_cities:# key=distance_from_current_city
+            next_city=i
+        print (mini)
         unvisited_cities.remove(next_city)
         solution.append(next_city)
         current_city = next_city
+        print (solution)
     return solution
 
 
@@ -37,3 +47,4 @@ if __name__ == '__main__':
     assert len(sys.argv) > 1
     solution = solve(read_input(sys.argv[1]))
     print_solution(solution)
+
